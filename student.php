0c30e160ef2e74ea60ac590ad98b47266236dc52
angimeucci <accountstudente@mail.com>
2021-04-25 19:28:40 +0200

    2 miglioramenti metodi post e put, correzione metodo delete

--- student.php
+++ student.php
@@ -18,43 +18,46 @@ switch($method) {
     break;
 
   case 'POST':
+    $body = file_get_contents("php://input");
+    $js_decoded = json_decode($body, true);
+
     $student = new Student();
-    $id = $student->$id;
-    if(isset($id)){
-      $student->post($student);
-      $js_encode = json_encode(array('state'=>TRUE, 'student'=>$student),true);
-      header("Content-Type: application/json");
-      echo($js_encode);
-    }
+    $student->_name = $js_decoded["_name"];
+    $student->_surname = $js_decoded["_surname"];
+    $student->_sidiCode = $js_decoded["_sidiCode"];
+    $student->_taxCode = $js_decoded["_taxCode"];
+
+    $student->post($student);
+    $js_encode = json_encode(array('state'=>TRUE, 'student'=>$student),true);
+    header("Content-Type: application/json");
+    echo($js_encode);
     break;
 
   case 'DELETE':
-    $id = $_DELETE['id'];
+    $id = $_GET['id'];
     if(isset($id))
     {
-      $student = $student->find($id);
-      if($student!=null)
-      {
-        $student->delete($id);
-        $js_encode = json_encode(array('state'=>TRUE, 'student'=>$student),true);
-      }
+      if($student->delete($id) <= 1)
+              $js_encode = json_encode(array('state'=>TRUE, 'student'=>$student),true);
       header("Content-Type: application/json");
       echo($js_encode);
     }
     break;
 
   case 'PUT':
+    $body = file_get_contents("php://input");
+    $js_decoded = json_decode($body, true);
+
     $student = new Student();
-    $id = $student->$id;
-    if(isset($id)){
-      $search = $student->find($id);
-      if($search == null){
-        $student->put($student);
-        $js_encode = json_encode(array('state'=>TRUE,'student'=>$student),true);
-      }
-      header("Content-Type: application/json");
-      echo($js_encode);
-    }
+    $student->_name = $js_decoded["_name"];
+    $student->_surname = $js_decoded["_surname"];
+    $student->_sidiCode = $js_decoded["_sidiCode"];
+    $student->_taxCode = $js_decoded["_taxCode"];
+    
+    $student->put($student);
+    $js_encode = json_encode(array('state'=>TRUE,'student'=>$student),true);
+    header("Content-Type: application/json");
+    echo($js_encode);
     break;
 
   default:
